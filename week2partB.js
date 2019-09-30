eebfbe2edd1ad615e1a6050a3788f30060925bc6
reenesa <susannins@gmail.com>
2019-09-30 03:30:42 +0300

    implement object function

--- week2partB.js
+++ week2partB.js
@@ -1,16 +1,17 @@
 //the issue is I want to change num to incrementing numbers
 var squareArray=[];
 var object2=[]
-for(num=2; num<15; num++){    
+for(num=1; num<15; num++){    
 var object ={
     // i want num(key):to be equal to num(value)
-    2 :num*2
+    1 :num*2
     
 }
-object[num]=object[2]
+object[num]=object[1]
 object2.push(object);
-delete object[2];
 object[num+1];
+delete object[1];
+
 
 }
 
